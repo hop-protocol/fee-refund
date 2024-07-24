68f1711b9b70cc9065b940d1754b120756de81c1
Miguel Mota <hello@miguelmota.com>
2024-07-24 16:45:08 -0700

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export * from './feeRefund.js'
-
+import { getNetwork } from '@hop-protocol/sdk'
 import { goerli as goerliAddresses, mainnet as mainnetAddresses } from '@hop-protocol/sdk/addresses'
+
+export * from './feeRefund.js'
 
 export const networks = {
   mainnet: mainnetAddresses,
@@ -11,3 +12,5 @@ export const addresses = {
   goerli: goerliAddresses,
   mainnet: mainnetAddresses
 }
+
+export { getNetwork }
